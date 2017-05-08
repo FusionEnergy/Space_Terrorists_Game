1d760713534bb3ba252d57d5d122c46000f968ef
FusionEnergy <yiduowang12345@gmail.com>
2017-05-08 16:58:19 -0400

    Arranged missile projectiles into a class.

--- Our-First-Game/Game1.cs
+++ Our-First-Game/Game1.cs
@@ -7,54 +7,38 @@ using Microsoft.Xna.Framework.Audio;
 
 namespace Our_First_Game
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
         private Texture2D stars, cruiser, scorpion, rocketShot1, rocketShot2;
+        private ProjectileFireRight cruFireRight;
         private SoundEffect rocketSound;
         private KeyboardState keyOldState;
         private SpriteFont font;
-        private bool shot1 = false, shot2 = false;
         private int score = 0;
-        private float cruXPos = 50, cruYPos = 380, scoXPos = 700, scoYPos = 60, rocketStartX1, rocketStartY1, rocketStartX2, rocketStartY2, rocketSpeed1 = 0, rocketSpeed2 = 0;
+        private float cruXPos = 50, cruYPos = 380, scoXPos = 700, scoYPos = 60;
+        public static bool shot1 = false;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-
-            this.Window.Title = "The Adventure of The Future: Space Terrorists";
-
             IsFixedTimeStep = false;
 
+            this.Window.Title = "The Adventure of The Future: Space Terrorists";
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             this.IsMouseVisible = true;
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             cruiser = Content.Load<Texture2D>("Pictures/cruiser");
@@ -72,30 +56,17 @@ namespace Our_First_Game
             rocketSound = Content.Load<SoundEffect>("Sounds/SoundFX/rocket_sound");
 
             font = Content.Load<SpriteFont>("Fonts/Score");
-
-            // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
-
-            // TODO: Add your update logic here
 
             KeyboardState keyNewState = Keyboard.GetState();
 
@@ -115,17 +86,12 @@ namespace Our_First_Game
             {
                 cruXPos -= 1.8f;
             }
-            if (keyNewState.IsKeyDown(Keys.F)) // if (keyOldState.IsKeyUp(Keys.F) && keyNewState.IsKeyDown(Keys.F))
-            {//thinking of creating a function for this rocket shot, maybe in another class even
-                shot1 = true;
-                rocketStartX1 = cruXPos + 19; //don't know why rocket doesn't start shooting at cruiser sprite
-                rocketStartY1 = cruYPos + 26;
-            }
-            if (rocketStartX1 + rocketSpeed1 >= 800)
+            if (keyOldState.IsKeyUp(Keys.F) && keyNewState.IsKeyDown(Keys.F))
             {
-                shot1 = false;
+                shot1 = true;
+                cruFireRight = new ProjectileFireRight(rocketShot1, cruXPos + 19, cruYPos + 26);
+                rocketSound.Play(0.1f, 0, 0);
             }
-            rocketSpeed1 += 6;
 
             if (keyNewState.IsKeyDown(Keys.Up))
             {
@@ -149,10 +115,6 @@ namespace Our_First_Game
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             float frameRate = 1 / (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -169,14 +131,13 @@ namespace Our_First_Game
 
             if (shot1 == true)
             {
-                spriteBatch.Draw(rocketShot1, new Vector2(rocketStartX1 + rocketSpeed1, rocketStartY1), Color.White);
+                cruFireRight.Draw(spriteBatch);
             }
 
             spriteBatch.DrawString(font, "Score: " + score, new Vector2(50, 30), Color.White);
             spriteBatch.DrawString(font, frameRateOutput, new Vector2(750, 0), Color.Yellow);
 
             spriteBatch.End();
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
---
+++ Our-First-Game/ProjectileFireRight.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Our_First_Game
+{
+    class ProjectileFireRight
+    {
+        private Texture2D rocketShot1;
+        private float rocketStartX1, rocketStartY1, rocketSpeed1;
+
+        public ProjectileFireRight(Texture2D projectile, float startX, float startY)
+        {
+            rocketShot1 = projectile;
+            rocketStartX1 = startX;
+            rocketStartY1 = startY;
+        }
+
+        private bool rocketEnd()
+        {
+            rocketSpeed1 += 15;
+            if (rocketStartX1 + rocketSpeed1 >= 1000)
+            {
+                Game1.shot1 = false;
+                return false;
+            }
+            return true;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (rocketEnd())
+            {
+                spriteBatch.Draw(rocketShot1, new Vector2(rocketStartX1 + rocketSpeed1, rocketStartY1), Color.White);
+            }
+        }
+    }
+}
