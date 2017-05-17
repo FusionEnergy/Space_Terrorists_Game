6770ff100dd9601b8c3c48ee36e5c523566eb526
FusionEnergy <yiduowang12345@gmail.com>
2017-05-17 15:24:07 -0400

    Added borders

--- Our-First-Game/Game1.cs
+++ Our-First-Game/Game1.cs
@@ -12,7 +12,7 @@ namespace Our_First_Game
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        private Texture2D space, cruiser, scorpion, rocketShot1, rocketShot2;
+        private Texture2D space, cruiser, scorpion, rocketShot1, rocketShot2, borderPixel;
         private Rectangle cruRect, scoRect;
         private ProjectileFireRight cruFireRight;
         private ProjectileFireLeft scoFireLeft;
@@ -20,7 +20,7 @@ namespace Our_First_Game
         private KeyboardState keyOldState;
         private SpriteFont font, tuganoFont;
         private int score1 = 0, score2 = 0;
-        private float cruXPos = 50, cruYPos = 380, scoXPos = 700, scoYPos = 60, reload1 = 0, reload2 = 0;
+        private float cruXPos = 50, cruYPos = 380, scoXPos = 700, scoYPos = 80, reload1 = 0, reload2 = 0;
         private bool cruGracePeriod = true, scoGracePeriod = true;
         public static bool shot1 = false, shot2 = false;
 
@@ -43,6 +43,9 @@ namespace Our_First_Game
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            borderPixel = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            borderPixel.SetData(new[] { Color.White });
 
             cruiser = Content.Load<Texture2D>("Pictures/cruiser");
             scorpion = Content.Load<Texture2D>("Pictures/scorpion");
@@ -72,8 +75,6 @@ namespace Our_First_Game
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            Console.WriteLine("$");
-
             reload1 += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
             reload2 += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
 
@@ -84,32 +85,36 @@ namespace Our_First_Game
             {
                 score1++;
                 scoGracePeriod = true;
-                Console.WriteLine("$: rocket hit! BLUE");
+                Console.WriteLine(gameTime.TotalGameTime.TotalSeconds + ": rocket hit! BLUE");
             }
 
             if (ProjectileFireLeft.rocketBox2.Intersects(cruRect) && !cruGracePeriod)
             {
                 score2++;
                 cruGracePeriod = true;
-                Console.WriteLine("$: rocket hit! RED");
+                Console.WriteLine(gameTime.TotalGameTime.TotalSeconds + ": rocket hit! RED");
             }
 
             KeyboardState keyNewState = Keyboard.GetState();
 
             if (keyNewState.IsKeyDown(Keys.W))
             {
+                if (cruYPos >= 70)
                 cruYPos -= 3;
             }
             if (keyNewState.IsKeyDown(Keys.S))
             {
+                if (cruYPos <= 480 - cruiser.Height)
                 cruYPos += 3;
             }
             if (keyNewState.IsKeyDown(Keys.D))
             {
+                if (cruXPos <= 395 - cruiser.Width)
                 cruXPos += 2.3f;
             }
             if (keyNewState.IsKeyDown(Keys.A))
             {
+                if (cruXPos >= 0)
                 cruXPos -= 1.8f;
             }
             if (keyOldState.IsKeyUp(Keys.Q) && keyNewState.IsKeyDown(Keys.Q) && reload1 >= 1200)
@@ -123,18 +128,22 @@ namespace Our_First_Game
 
             if (keyNewState.IsKeyDown(Keys.I))
             {
+                if (scoYPos >= 70)
                 scoYPos -= 3;
             }
             if (keyNewState.IsKeyDown(Keys.K))
             {
+                if (scoYPos <= 480 - scorpion.Height)
                 scoYPos += 3;
             }
             if (keyNewState.IsKeyDown(Keys.J))
             {
+                if (scoXPos >= 405)
                 scoXPos -= 2.3f;
             }
             if (keyNewState.IsKeyDown(Keys.L))
             {
+                if (scoXPos <= 800 - scorpion.Width)
                 scoXPos += 1.8f;
             }
             if (keyOldState.IsKeyUp(Keys.U) && keyNewState.IsKeyDown(Keys.U) && reload2 >= 1200)
@@ -153,6 +162,8 @@ namespace Our_First_Game
 
         protected override void Draw(GameTime gameTime)
         {
+            string reload = "Reloading...";
+
             float frameRate = 1 / (float)gameTime.ElapsedGameTime.TotalSeconds;
             string frameRateOutput = Math.Round(frameRate).ToString();
 
@@ -161,6 +172,9 @@ namespace Our_First_Game
             spriteBatch.Begin();
 
             spriteBatch.Draw(space, new Rectangle(0, 0, 800, 480), Color.White);
+
+            spriteBatch.Draw(borderPixel, new Rectangle(0, 68, 800, 3), Color.White);
+            spriteBatch.Draw(borderPixel, new Rectangle(400, 68, 3, 412), Color.White);
 
             spriteBatch.Draw(cruiser, new Vector2(cruXPos, cruYPos), Color.White);
             spriteBatch.Draw(scorpion, new Vector2(scoXPos, scoYPos), Color.White);
@@ -176,17 +190,17 @@ namespace Our_First_Game
             }
 
             spriteBatch.DrawString(font, frameRateOutput, new Vector2(750, 0), Color.Yellow);
-            spriteBatch.DrawString(font, ":", new Vector2(400, 27), Color.White);
+            spriteBatch.DrawString(font, ":", new Vector2(398.5f, 27), Color.White);
             string tempScore1 = Convert.ToString(score1), tempScore2 = Convert.ToString(score2);
-            spriteBatch.DrawString(font, tempScore1, new Vector2(375, 27), Color.Blue);
-            spriteBatch.DrawString(font, tempScore2, new Vector2(425, 27), Color.Red);
+            spriteBatch.DrawString(font, tempScore1, new Vector2(355, 27), Color.Blue);
+            spriteBatch.DrawString(font, tempScore2, new Vector2(435, 27), Color.Red);
             if (reload1 < 1200)
             {
-                spriteBatch.DrawString(tuganoFont, "Reloading...", new Vector2(105, 32), Color.Blue);
+                spriteBatch.DrawString(tuganoFont, reload, new Vector2(55, 32), Color.Blue);
             }
             if (reload2 < 1200)
             {
-                spriteBatch.DrawString(tuganoFont, "Reloading...", new Vector2(615, 32), Color.Red);
+                spriteBatch.DrawString(tuganoFont, reload, new Vector2(745 - tuganoFont.MeasureString(reload).X, 32), Color.Red);
             }
 
             spriteBatch.End();
